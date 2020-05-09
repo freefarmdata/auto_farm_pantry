f554342765ffe0213491718dee911dc52ee184bd
jack <jackmead515@gmail.com>
2020-05-09 11:27:59 -1000

    updating live:

--- src/services/live.js
+++ src/services/live.js
@@ -3,21 +3,31 @@ const proto = require('auto_farm_protos');
 
 const logger = require('../util/logger');
 
+function parseBody(data) {
+  const headers = Buffer.from(data.data.slice(0, 8));
+  const buffer = Buffer.from(data.data.slice(8, data.data.length));
+
+  const software = headers.slice(0, 4).readInt32LE();
+  const version = headers.slice(4, 8).readInt32LE();
+
+  return { buffer, software, version };
+}
+
+function parseProto(state) {
+  const { version, buffer } = state;
+
+  const message = proto.readState(version, buffer);
+
+  logger.log(`Proto State: `, JSON.stringify(message));
+
+  return { ...state, message };
+}
+
 route.post('/', function(req, res) {
   const { data } = req.body;
 
-  const headers = Buffer.from(data.data.slice(0, 8));
-  const buffer = Buffer.from(data.data.slice(8, data.data.length));
-  const software = Buffer.from(headers.slice(0, 4)).readInt32LE();
-  const version = Buffer.from(headers.slice(4, 8)).readInt32LE();
-
-  logger.log(`Incoming Data Version: ${software}.${version}`, buffer.length);
-
   try {
-    const state = proto.readState(version, buffer);
-
-    logger.log(state);
-
+    const state = parseProto(parseBody(data));
     return res.status(200).send();
   } catch (err) {
     logger.log(err);
